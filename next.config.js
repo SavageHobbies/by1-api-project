15f994b3a76dfa39f85e097675219a3837009146
Hollywood <112442093+SavageHobbies@users.noreply.github.com>
2024-12-19 01:56:40 -0500

    Front end return

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '/api',
+  },
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
